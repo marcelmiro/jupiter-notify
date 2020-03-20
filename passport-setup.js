9f2e4ec804a5bbaf69cb3d7175f5a5c773182cc6
Marcel <mmp534@york.ac.uk>
2020-03-20 16:36:10 +0000

    DiscordStrategy callbackURL change

--- passport-setup.js
+++ passport-setup.js
@@ -18,7 +18,7 @@ passport.use(new DiscordStrategy(
     {
         clientID: process.env.DISCORD_CLIENT_ID,
         clientSecret: process.env.DISCORD_CLIENT_SECRET,
-        callbackURL: "/auth/redirect"
+        callbackURL: "https://jupiter-notify.herokuapp.com/auth/redirect"
     },
     /**
      * Callback function
