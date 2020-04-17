fc280600ba8e884f486ab67c71250b577d5e4c5e
Marcel <mmp534@york.ac.uk>
2020-04-17 23:45:15 +0200

    Trying heroku https server

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ dbUtils.setSettings().then(() => {
             key: fs.readFileSync("./ssl/localhost-key.pem"),
             cert: fs.readFileSync("./ssl/localhost-cert.pem")
         }, app);
-    } else { server = require("https").createServer(app); }
+    } else { server = require("http").createServer(app); }
 
     server.listen(port,() => {
         console.log(`Server connected at: ${port}`);
