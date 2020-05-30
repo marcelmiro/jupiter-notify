2cd9d7a4d84641779fe362b9d18bbe75dd409445
Marcel <mmp534@york.ac.uk>
2020-05-30 16:37:06 +0200

    Discord bot debugging.

--- utils/bot-utils.js
+++ utils/bot-utils.js
@@ -9,8 +9,10 @@ let guild;
 client.on("ready",() => {
     guild = client.guilds.cache.get(process.env.DISCORD_GUILD_ID);
     console.log(`Bot logged in as ${client.user.tag}.`);
-    let CHANNEL_ID = guild.channels.cache;
+    let CHANNEL_ID = guild.channels.cache.first().guild.systemChannelID;
     console.log(CHANNEL_ID);
+    console.log(guild.channels.cache[0].id);
+    console.log(guild.channels.cache.get("716231055644033034"));
     //console.log(client.channels.cache.get(CHANNEL_ID));
 });
 
